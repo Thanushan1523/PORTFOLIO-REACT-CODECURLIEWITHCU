9b750738c61cdf5ce032fc22135900b6aeb8d7c2
Thanushan1523 <thanushan1523@gmail.com>
2024-12-10 08:16:46 -0500

    Projects & about page -video-6

--- src/routes/Pro.jsx
+++ src/routes/Pro.jsx
@@ -1,8 +1,15 @@
+
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 const Pro = () => {
   return (
-    <div>Pro</div>
+    <div>
+          <NavBar/>
+            <Footer/>
+    </div>
+    
   )
 }
 
