7109a53a1dfd408e31f2429020f6dc022cf1ea22
Thanushan1523 <thanushan1523@gmail.com>
2024-12-11 07:44:56 -0500

    home.jsx & HomeStyles.css is created -video-6-completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,9 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className='heading'>
+      <h1> hi  hff  </h1>
         <p>thid is praga</p>
-        <h1> hi  hff  </h1>
+        
         </div> 
 
     </div>
