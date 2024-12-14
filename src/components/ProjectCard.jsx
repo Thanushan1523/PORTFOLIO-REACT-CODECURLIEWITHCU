5316a6d7da320d1434af779a026dc4784aed0515
Thanushan1523 <thanushan1523@gmail.com>
2024-12-14 08:21:55 -0500

    dynamic home page created-video 8

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// import "./ProjectCardStyles.css"
+import "./Test.css"
+import pro1 from "../assets/project1.jpg"
+import { NavLink } from 'react-router-dom'
+
+const ProjectCard = () => {
+  return (
+    <div classname="work-container"> 
+    <h1 className='project-heading'>hddyybyytt
+        </h1>
+        <div className='project-container'>
+            <div className='project-card'>
+                <img src={pro1} alt="ff"/>
+                <h2 className='project-title'>project tittle</h2>
+                <div className='project-details'>
+                <p>This is text</p>
+                <div className='pro-btns'>
+                    <NavLink to="url.com" className="btn">view</NavLink>
+                    <NavLink to="url.com" className="btn">source</NavLink>
+                </div>
+                </div>
+            </div>
+            </div></div>
+  )
+}
+
+export default ProjectCard;
--- src/routes/Pro.jsx
+++ src/routes/Pro.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
+import ProjectCard from '../components/ProjectCard'
 const Pro = () => {
   return (
     <div>
           <NavBar/>
           <Hero  heading="PROJECTS" text="this is few projects" />
+            <ProjectCard/>
             <Footer/>
             
     </div>
