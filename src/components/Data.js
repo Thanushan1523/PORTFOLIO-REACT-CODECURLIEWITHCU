c3bd06f47b6a97a9acd782fdad930e2d70012bc7
Thanushan1523 <thanushan1523@gmail.com>
2024-12-15 16:24:29 -0500

    simplfy the projectcard.jsx- created workcard.jsx-video -9

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,23 @@
 import pro1 from "../assets/project1.jpg"
-
-const ProjectData={
-    imgsrc:pro1
-}
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/project3.jpg"
+const ProjectData=[
+    {
+    imgsrc:pro1,
+    title:"jnjgbbkbkbbgbgk",
+    text:"skjcfbkjbkjbkdgumgunu",
+    view:"https://github.com/Thanushan1523/PORTFOLIO-REACT-CODECURLIEWITHCU"
+},
+{
+    imgsrc:pro2,
+    title:"jnjgbbkbkbbgbgk",
+    text:"skjcfbkjbkjbkdgumgunu",
+    view:"https://github.com/Thanushan1523/PORTFOLIO-REACT-CODECURLIEWITHCU"
+},
+{
+    imgsrc:pro3,
+    title:"jnjgbbkbkbbgbgk",
+    text:"skjcfbkjbkjbkdgumgunu",
+    view:"https://github.com/Thanushan1523/PORTFOLIO-REACT-CODECURLIEWITHCU"
+},
+]
--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -8,7 +8,7 @@ const WorkCard = () => {
   return (
     
             <div className='project-card'>
-                <img src={pro1} alt="ff"/>
+                <img src={props.imgsrc} alt="ff"/>
                 <h2 className='project-title'>project tittle</h2>
                 <div className='project-details'>
                 <p>This is texruuruyruy utiiiiiiiiiiiiiiii toooooooooooooooooooo tooooooooooooooo uttttttt utttttttttttttttttttttttttttttoott</p>
