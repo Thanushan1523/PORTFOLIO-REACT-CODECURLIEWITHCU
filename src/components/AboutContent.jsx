c67284d03a903ef81184424bfe0560375932cec6
Thanushan1523 <thanushan1523@gmail.com>
2024-12-17 07:08:17 -0500

    Developing about page -video -11-competed

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -10,7 +10,7 @@ const AboutContent = () => {
         <div className="left">
             <h1> who am i ?</h1>
             <p>uagbcu uruicam urgai  aeeo  e ya8c  8yacpmgjiut r8tv y8tr ybyu jdyjd ss ituori tu vi </p>
-            <Link to ="/contact"><button className="btn">Contact</button></Link>
+            <Link to ="/contacts"><button className="btn">Contact</button></Link>
         </div>
 
 
@@ -18,6 +18,10 @@ const AboutContent = () => {
 <div className="img-container">
     <div className="img-stack top">
         <img src={React1} className="img" alt="React"/>
+       
+    </div>
+    <div className="img-stack bottom">
+        
         <img src={React2} className="img" alt="React"/>
     </div>
 
