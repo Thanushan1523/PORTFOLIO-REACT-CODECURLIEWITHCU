00ceb9e398cfc9cc3396352cd42c1e32d226f85b
Thanushan1523 <thanushan1523@gmail.com>
2024-12-18 07:08:34 -0500

    Developing the contact page -created Form.jsx &css -video -12

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -3,11 +3,14 @@ import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 
+import Form from '../components/Form'
 const Contacts = () => {
   return (
     <div>
      <NavBar/>
      <Hero heading="about me"   text="0777 888 888 88 "/>
+     <Form/>
+
      <Footer/>
    
             </div>
